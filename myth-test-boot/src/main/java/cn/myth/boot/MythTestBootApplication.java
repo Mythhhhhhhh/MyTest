feca6e971f5c4f529e9c7ed320b4df6fcf0254b1
myth <574630686@qq.com>
2025-02-13 00:58:27 +0800

    add

---
+++ myth-test-boot/src/main/java/cn/myth/boot/MythTestBootApplication.java
@@ -0,0 +1,13 @@
+package cn.myth.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MythTestBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MythTestBootApplication.class, args);
+    }
+
+}
---
+++ myth-test-boot/src/test/java/cn/myth/boot/MythTestBootApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.myth.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MythTestBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
