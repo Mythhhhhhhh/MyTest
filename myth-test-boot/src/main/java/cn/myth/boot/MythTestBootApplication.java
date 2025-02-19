744aaef3ce0ea6bf037b36a71f431f49c02a3204
myth <574630686@qq.com>
2025-02-20 01:31:12 +0800

    add

--- myth-test-boot/src/main/java/cn/myth/boot/MythTestBootApplication.java
+++ myth-test-boot/src/main/java/cn/myth/boot/MythTestBootApplication.java
@@ -1,9 +1,11 @@
 package cn.myth.boot;
 
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Configurable
 public class MythTestBootApplication {
 
     public static void main(String[] args) {
